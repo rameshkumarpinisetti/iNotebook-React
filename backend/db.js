5de40b066e3f6c016be349f42f056cbe8abc1b4e
rameshkumarpinisetti <pinisettirameshkumar@gmail.com>
2025-01-19 16:08:56 +0530

    code update

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log('connected to mongoose successfully');
+        })
+        .catch((err) => {
+            console.error('Error connecting to mongoose:', err);
+        });
+}
+
+module.exports = connectToMongo;
